2f5b45ce3e19476454bdd88a1812a07c67a0e630
João Paulo Carvalho <jjpaulo2@protonmail.com>
2026-05-15 15:38:28 -0300

    feat: new outputs

--- terraform/production/outputs.tf
+++ terraform/production/outputs.tf
@@ -6,6 +6,10 @@ output "node_0_private_ip" {
   value = module.node_0.private_ip
 }
 
+output "node_0_instance_id" {
+  value = module.node_0.instance_id
+}
+
 output "node_0_ssh_private_key" {
   value     = module.node_0.ssh_private_key
   sensitive = true
@@ -19,6 +23,10 @@ output "node_1_private_ip" {
   value = module.node_1.private_ip
 }
 
+output "node_1_instance_id" {
+  value = module.node_1.instance_id
+}
+
 output "node_1_ssh_private_key" {
   value     = module.node_1.ssh_private_key
   sensitive = true
@@ -30,6 +38,10 @@ output "node_heavy_0_public_ip" {
 
 output "node_heavy_0_private_ip" {
   value = module.node_heavy_0.private_ip
+}
+
+output "node_heavy_0_instance_id" {
+  value = module.node_heavy_0.instance_id
 }
 
 output "node_heavy_0_ssh_private_key" {
