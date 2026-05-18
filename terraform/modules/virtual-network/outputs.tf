571a149e1dcb8243d7a5d2d9311e8bf5953dddbd
João Paulo Carvalho <jjpaulo2@protonmail.com>
2026-05-18 11:31:28 -0300

    feat: output cidr blocks

--- terraform/modules/virtual-network/outputs.tf
+++ terraform/modules/virtual-network/outputs.tf
@@ -7,3 +7,13 @@ output "subnet_id" {
   description = "OCID da subnet criada"
   value       = oci_core_subnet.this.id
 }
+
+output "vcn_cidr" {
+  description = "CIDR da VCN criada"
+  value       = oci_core_vcn.this.cidr_block
+}
+
+output "subnet_cidr" {
+  description = "CIDR da subnet criada"
+  value       = oci_core_subnet.this.cidr_block
+}
